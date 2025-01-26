25972b1f99c26c1f19cf202e4b9d86bb10f4d6fa
randyviryawan24 <randyviryawan24@gmail.com>
2025-01-26 14:23:32 +0930

    Remove old SQL and dataset files

--- project_1/Create_Table/modify_table.sql
+++
@@ -1,3 +0,0 @@
-
-\copy ecommerce_data FROM 'C:\Users\Randy V\SQL_Project_Data_Job_Analysis\project_1\dataset\ecommerce_data.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-\copy longitude_Latitude_data FROM 'C:\Users\Randy V\SQL_Project_Data_Job_Analysis\project_1\dataset\longitude_Latitude_data.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
